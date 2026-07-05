1eb537c9756fbbcc8e60c5972b73600c2d5b1617
bereeza <timofejberezanskij@gmail.com>
2026-07-05 19:46:26 +0300

    [CA-8:PATCH-1] Moved & Updated Exception Handler.

--- carbon-ai-service/src/main/java/com/carbon/ai/CarbonAiServiceApplication.java
+++ carbon-ai-service/src/main/java/com/carbon/ai/CarbonAiServiceApplication.java
@@ -2,8 +2,10 @@ package com.carbon.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.carbon.ai", "com.carbon.shared"})
 public class CarbonAiServiceApplication {
 
     public static void main(String[] args) {
--- carbon-ingestion-service/src/main/java/com/carbon/ingestion/CarbonIngestionServiceApplication.java
+++ carbon-ingestion-service/src/main/java/com/carbon/ingestion/CarbonIngestionServiceApplication.java
@@ -2,8 +2,10 @@ package com.carbon.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.carbon.ingestion", "com.carbon.shared"})
 public class CarbonIngestionServiceApplication {
 
     public static void main(String[] args) {
